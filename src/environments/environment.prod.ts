b457dac2e1613addb88a4ed8544595c7391026de
MuSabahAllah <musabahallah@gmail.com>
2022-08-27 22:40:18 +0200

    Remove Expired API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,3 @@
-// import { environment as common } from './environment';
-
 export const environment = {
-  // ...common,
   production: true,
-
-  BASE_URL: 'https://yahoo-weather5.p.rapidapi.com/weather?',
-  XRapidAPIHost: 'X-RapidAPI-Host',
-  XRapidAPIHostValue: 'yahoo-weather5.p.rapidapi.com',
-  XRapidAPIKey: 'X-RapidAPI-Key',
-  XRapidAPIKeyValue: '5e7b2f6b35mshb113912c3c97dfcp1af9fejsn2fdfb8970046',
-
-  API_KEY: 'ef843dd7e1561a59e0ed984801612c11',
 };
